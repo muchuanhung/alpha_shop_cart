86aeb5ed2d39dcff1ce5458a67ebd7866809d605
muchuan <mu.chuan.hung@gmail.com>
2021-11-04 16:05:48 +0800

    final retouch

--- README.md
+++ README.md
@@ -11,28 +11,6 @@ shop-cart-template
 - footer wouldn't show up on mobile size.
 - switch light and dark mode.
 
-## Prerequisites
-
-- Node.js
-- Express v4.17.1
-
-## Installation
-
-To run the project locally
-
-- Please clone it.
-  ```bash
-  git clone https://github.com/muchuanhung/alpha_shop_cart.git
-  ```
-- Install dependencies
-  ```bash
-  npm install
-  ```
-- Running Server (if server start to run, terminal will show: "Express running on the port 3000")
-
-  - node.js -> <code>$ npm run start</code>
-  - nodemon -> <code>$ npm run dev</code>
-
 ## Development by
 
 - HTML
@@ -40,6 +18,4 @@ To run the project locally
 - JavaScript (ES6)
 - Flex/Grid
 - RWD
-- Node.js
-- Express
-- Nodemon
+- Scss
--- main.js
+++ main.js
@@ -1,206 +1,144 @@
-// ------ Variables -------- //
-const userShoppingList = [
-  {
-    id: 1,
-    productName: '破壞補丁修身牛仔褲',
-    productImg: './src/image/product_01.jpg',
-    unitPrice: 3999,
-    purchaseQty: 1,
-  },
-  {
-    id: 2,
-    productName: '刷色直筒牛仔褲',
-    productImg: './src/image/product_02.jpg',
-    unitPrice: 1299,
-    purchaseQty: 1,
-  }
-]
-let indexActiveForm = 0
+const themeDark = document.getElementsByClassName("theme-mode-dark");
+const themeLight = document.getElementsByClassName("theme-mode-light");
+const themeDarkLg = document.getElementById("theme-mode-dark");
+const themeLightLg = document.getElementById("theme-mode-light");
+const form = document.getElementById("a-form");
+const formParts = form.querySelectorAll(".part");
+const stepControl = document.getElementById("step-control");
+const steps = stepControl.querySelectorAll(".step");
+const btnControl = document.getElementById("btn-control");
+const nextBtn = btnControl.querySelector(".btn-next");
+const prevBtn = btnControl.querySelector(".btn-prev");
+const cartOrder = document.getElementById("cart-order");
+const amountTotal = document.getElementById("amount-total");
+const shippingFee = document.getElementById("shipping-fee");
+const chargeType = document.getElementById("charge-type");
+const commonFee = shippingFee.firstElementChild.lastElementChild;
+const dhlFee = shippingFee.lastElementChild.lastElementChild;
 
-// --------- Nodes ---------- //
-const headerActions = document.querySelector('.header-actions')
-const darkModeToggle = headerActions.querySelector('#dark-mode-toggle')
-const stepper = document.querySelector('.steps-wrapper')
-const stepList = stepper.querySelectorAll('.step')
-const progressLine = stepper.querySelector('.progress-line')
-const formGroup = document.querySelector('.form-group')
-const formList = formGroup.querySelectorAll('.form-part')
-const formOfShipping = formGroup.querySelector('.form-part-shipping')
-const btnGroup = document.querySelector('.btn-groups')
-const btnPrevious = btnGroup.querySelector('.btn-previous')
-const btnNext = btnGroup.querySelector('.btn-next')
-const btnSubmit = btnGroup.querySelector('.btn-submit')
-const shoppingCart = document.querySelector('.shopping-cart')
-const shoppingList = shoppingCart.querySelector('.shopping-list')
+let step = 0; //計算步驟0 1 2
+let total = 5298;
+let type = "";
 
-// ------- Functions -------- //
+// amountTotal.innerHTML = '$ ' + `${total.toString().replace(/(\d)(?=(?:\d{3})+$)/g,"$1,")}`
+amountTotal.innerHTML = "$ " + new Intl.NumberFormat().format(total);
 
-// Functions related to Event handle
-function handleBtnsClick(event) {
-  event.preventDefault()
-  const targetItem = event.target
-  
-  // back to previous stage
-  if (targetItem.classList.contains('btn-previous') && indexActiveForm > 0) {
-    const nextForm = formList[indexActiveForm - 1]
-    updateFormPart(nextForm)
-    indexActiveForm--
-    changeBtnStyle(indexActiveForm)
-    changeStepperStyle(indexActiveForm)
-    updateProgressLine(indexActiveForm, formList.length)
+function purchaseTotal(e) {
+  e.preventDefault();
 
-  // go to next step
-  } else if (targetItem.classList.contains('btn-next') && indexActiveForm < formList.length - 1) {
-    const nextForm = formList[indexActiveForm + 1]
-    updateFormPart(nextForm)
-    indexActiveForm++
-    changeBtnStyle(indexActiveForm)
-    changeStepperStyle(indexActiveForm)
-    updateProgressLine(indexActiveForm, formList.length)
-
-  // submit form
-  } else if (targetItem.classList.contains('btn-submit') && indexActiveForm === formList.length - 1) {
-    alert('訂單已送出')
-    window.location.reload()
+  let price =
+    e.target.parentElement.parentElement.nextElementSibling.firstElementChild
+      .textContent;
+  console.log(price);
+  if (e.target.matches(".goods-loss")) {
+    let count = e.target.nextElementSibling.value;
+    if (count > 1) {
+      e.target.nextElementSibling.value--;
+      total = total - Number(price);
+    }
+    amountTotal.innerHTML = "$ " + new Intl.NumberFormat().format(total);
+  } else if (e.target.matches(".goods-add")) {
+    let count = e.target.previousElementSibling.value++;
+    total = total + Number(price);
+    amountTotal.innerHTML = "$ " + new Intl.NumberFormat().format(total);
   }
 }
 
-function handleQtyAdjustment(event) {
-  const targetItem = event.target
-  if (targetItem.matches('.qty-controller')) {
-    const shoppingItem = targetItem.closest('.shopping-item')
-    updateProductQty(targetItem, shoppingItem)
-    updateSubtotal(shoppingItem)
-    updateShoppingAmount() 
+function handleBtnControlClicked(e) {
+  e.preventDefault();
+  const nowStep = steps[step];
+  if (e.target.matches(".btn-next")) {
+    if (step === 2) {
+      return;
+    }
+    const nextStep = steps[step + 1];
+    nowStep.classList.remove("steps-item-process");
+    nowStep.classList.add("steps-item-done");
+    nextStep.classList.add("steps-item-process");
+    formParts[step].classList.toggle("d-none");
+    formParts[step + 1].classList.toggle("d-none");
+    step += 1;
+  } else if (e.target.matches(".btn-prev")) {
+    const prevStep = steps[step - 1];
+    nowStep.classList.remove("steps-item-process");
+    prevStep.classList.remove("steps-item-done");
+    prevStep.classList.add("steps-item-process");
+    formParts[step].classList.toggle("d-none");
+    formParts[step - 1].classList.toggle("d-none");
+    step -= 1;
   }
+  setBtnDisabled();
 }
 
-function toggleDarkMode() {
-  const darkIcon = headerActions.querySelector('.fa-moon')
-  const lightIcon = headerActions.querySelector('.fa-sun')
-  
-  if (event.target.checked) {
-    document.documentElement.setAttribute('dark-theme', 'dark')
+function setBtnDisabled() {
+  if (step === 0) {
+    prevBtn.setAttribute("disabled", "disabled");
   } else {
-    document.documentElement.setAttribute('dark-theme', 'light')
-  } 
-  darkIcon.classList.toggle('d-none')
-  lightIcon.classList.toggle('d-none')
-}
-
-// Functions related to Views
-const renderShoppingCart = function() {
-  userShoppingList.forEach(item => 
-    shoppingList.innerHTML += `
-      <div class="shopping-item d-flex justify-content-between" data-id=${item.id}>
-        <img src="${item.productImg}" alt="product-img" class="product-img">
-        <div class="item-info flex-grow-1 d-flex flex-column justify-content-between align-items-end">
-          <p class="product-name">${item.productName}</p>
-          <div class="qty-wrapper">
-            <span class="qty-controller qty-minus-circle d-inline-block rounded-circle">-</span>
-            <span class="qty mx-3">${item.purchaseQty}</span>
-            <span class="qty-controller qty-add-circle d-inline-block rounded-circle">+</span>
-          </div>
-          <div class="price-wrapper">
-            <span>$</span>
-            <span class="subtotal">${item.unitPrice}</span>
-          </div>
-        </div>
-      </div>
-    `
-    )
-  updateShoppingAmount()
-}()
-
-function updateFormPart(nextForm) {
-  const currentForm = formList[indexActiveForm]
-  currentForm.classList.add('d-none')
-  nextForm.classList.remove('d-none')
-}
-
-function changeBtnStyle(currentIndex) {
-  // change btn style according to current stage
-  switch (currentIndex) {
-    case 0 :
-      btnPrevious.classList.add('d-none')
-      break
-    case formList.length - 1:
-      btnNext.classList.add('d-none')
-      btnSubmit.classList.remove('d-none')
-      break
-    default:
-      btnPrevious.classList.remove('d-none')
-      btnNext.classList.remove('d-none')
-      btnSubmit.classList.add('d-none')
+    prevBtn.removeAttribute("disabled");
   }
+
+  // if (step === 2) {
+  //   nextBtn.innerHTML = "確認下單";
+  // } else {
+  //   nextBtn.innerHTML = "下一步 →";
+  // }
 }
 
-function changeStepperStyle(currentIndex) {
-  // change stepper style according to current step
-  stepList.forEach((step, stepIndex) => {
-    step.classList.remove('active')
-    if(stepIndex < currentIndex) {
-      step.classList.add('checked')
+function shippingTotal(e) {
+  if (e.target.matches(".mark")) {
+    total -= 500;
+    type = "免運";
+    commonFee.classList.remove("mark");
+    dhlFee.classList.add("mark2");
+  } else {
+    if (e.target.matches(".mark2")) {
+      total += 500;
+      type = "$500";
+      dhlFee.classList.remove("mark2");
+      commonFee.classList.add("mark");
+    } else {
+      total += 0;
     }
-  })
-  stepList[currentIndex].classList.remove('checked')
-  stepList[currentIndex].classList.add('active')
-}
-
-function updateProgressLine(currentIndex, totalStage) {
-  const lineWidth = currentIndex / (totalStage - 1) * 100
-  progressLine.style.width = `${lineWidth}%`
-}
-
-function updateProductQty(targetItem, shoppingItem) {
-  const qtyNode = shoppingItem.querySelector('.qty')
-  const currentQty = qtyNode.innerHTML
-  if (targetItem.matches('.qty-minus-circle') && currentQty > 0) {
-    qtyNode.innerHTML--
-  } else if (targetItem.matches('.qty-add-circle')) {
-    qtyNode.innerHTML++
   }
+  chargeType.innerText = type;
+  amountTotal.innerHTML = "$ " + new Intl.NumberFormat().format(total);
 }
 
-function updateSubtotal(shoppingItem) {
-  const productId = shoppingItem.dataset.id
-  // get unit price from DB
-  const targetProduct = userShoppingList.find(product => product.id.toString() === productId)
-  const unitPrice = targetProduct.unitPrice
-  const subtotal = shoppingItem.querySelector('.subtotal')
-  const qtyNode = shoppingItem.querySelector('.qty')
-  subtotal.innerHTML = qtyNode.innerHTML * unitPrice
-}
+btnControl.addEventListener("click", handleBtnControlClicked);
+shippingFee.addEventListener("click", shippingTotal);
+cartOrder.addEventListener("click", purchaseTotal);
 
-function updateShoppingAmount() {
-  const totalAmount = shoppingCart.querySelector('.total-amount')
-  const allSubtotal = shoppingCart.querySelectorAll('.subtotal')
-  let sum = 0
-  allSubtotal.forEach(subtotal => {
-    if (!isNaN(Number(subtotal.innerHTML))) { 
-    sum += Number(subtotal.innerHTML)
-    }
-  })
-  totalAmount.innerHTML = sum
-}
+themeDark[0].addEventListener("click", () => {
+  trans();
+  document.documentElement.setAttribute("data-theme", "dark");
+  themeDark[0].classList.add("d-none");
+  themeLight[0].classList.remove("d-none");
+});
 
-function updateShippingFee() {
-  const shippingSubtotal = shoppingCart.querySelector('.shipping-subtotal')
-  const shippingFee =  shippingSubtotal.querySelector('.subtotal')
-  const selectedShipping = event.target.parentElement.querySelector('.subtotal') 
-  shippingFee.innerHTML = selectedShipping.innerHTML
-  updateShoppingAmount()
-}
-// ------- Functions execution or invoked ------- //
+themeLight[0].addEventListener("click", () => {
+  trans();
+  document.documentElement.setAttribute("data-theme", "light");
+  themeDark[0].classList.remove("d-none");
+  themeLight[0].classList.add("d-none");
+});
 
-// Event listener for form page controls
-btnGroup.addEventListener('click', handleBtnsClick)
+themeDarkLg.addEventListener("click", () => {
+  trans();
+  document.documentElement.setAttribute("data-theme", "dark");
+  themeLightLg.classList.remove("d-none");
+  themeDarkLg.classList.add("d-none");
+});
 
-// Event listener for shipping fee selection
-formOfShipping.addEventListener('change', updateShippingFee)
+themeLightLg.addEventListener("click", () => {
+  trans();
+  document.documentElement.setAttribute("data-theme", "light");
+  themeDarkLg.classList.remove("d-none");
+  themeLightLg.classList.add("d-none");
+});
 
-// Event listener for detecting qty change of shopping items
-shoppingList.addEventListener('click', handleQtyAdjustment)
-
-// Event listener for dark mode shifting
-darkModeToggle.addEventListener('change', toggleDarkMode)
+let trans = () => {
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
+};
